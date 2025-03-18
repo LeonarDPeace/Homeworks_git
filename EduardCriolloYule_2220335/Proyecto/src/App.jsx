d321b6e75700292d986724c92a1703d45bd8df86
Leonar D. Peace <eduardcriolloyule2004@gmail.com>
2025-03-18 16:45:23 -0500

    Reinicio de Desarrollo
    Hubo muchos problemas para realizar el proyecto con el parcial como plantilla. Por lo que decidi empezar desde 0

--- EduardCriolloYule_2220335/Proyecto/src/App.jsx
+++ EduardCriolloYule_2220335/Proyecto/src/App.jsx
@@ -2,39 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ListaImagenes from './componentes/ListaImagenes'
-import BarraBusqueda from './componentes/BarraBusqueda'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const [images, setImages] = useState([]);
-  const [searchTitle, setSearchTitle] = useState('');
-
-  const controladorBtn = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-
-    const imageTitle = `Imagén #${newCount}`;
-
-    const URL = "https://picsum.photos/200/300?random=";
-    const imagenURL = `${URL}${newCount}`;
-    setImages([...images, {url:imagenURL, title:imageTitle}])
-  };
-
-  const filteredImages = images.filter((image) =>
-    image.title.includes(searchTitle)
-  );
-
   return (
     <>
       <div>
-        <h1>Parcial #1</h1>
-        <button onClick={controladorBtn}>Agregar Imagén</button>
-        <p>Numero de imagenes : {count}</p>
-        <BarraBusqueda setSearchTitle={setSearchTitle}/>
-        <ListaImagenes imagenes={filteredImages}/>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
